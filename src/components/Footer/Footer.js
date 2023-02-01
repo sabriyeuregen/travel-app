793b1e54aa126b4af6e889fb372e5e121a5cfbac
sabriyeuregen <sabriyeuregen@gmail.com>
2023-02-01 21:48:37 +0300

    Updated : Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,22 @@
 import "./Footer.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShield, faPhone, faMoneyCheck } from "@fortawesome/free-solid-svg-icons";
 const Footer = () => {
   return (
-    <div></div>
+    <div className="footer">
+      <div className="footer__security">
+      <FontAwesomeIcon className="icon" icon={faShield} />
+      <span>Tatiliniz güvende!</span>
+      </div>
+      <div className="footer__phone">
+      <FontAwesomeIcon className="icon" icon={faPhone} />
+      <span>444 444 44</span>
+      </div>
+      <div className="footer__money">
+      <FontAwesomeIcon className="icon" icon={faMoneyCheck} />
+      <span>Erken rezervasyon fırsatları</span>
+      </div>
+    </div>
   )
 }
 
